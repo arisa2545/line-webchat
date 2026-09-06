fd8f838af29b0a770d6fc35440e495a449d5f00e
Arisa <arisa.a@gofive.co.th>
2026-09-06 19:27:23 +0700

    handle empty user id on API get message by user id

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -2,6 +2,10 @@ import { getMessagesByUserId } from "@/lib/supabase/repository";
 
 export async function GET(req: Request) {
   const userId = new URL(req.url).searchParams.get("userId");
+  
+  if (!userId) {
+    return Response.json({ error: "Missing userId parameter" }, { status: 400 });
+  }
 
   try {
       return Response.json(await getMessagesByUserId(userId ?? ""));
